ed823882f55c6badb1a609d91ef381bf624a1cc1
JG <hwangje95@gmail.com>
2021-10-08 18:33:50 +0900

    commit

---
+++ week6/6_prac1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define COLS 3
+
+double get_row_avg(int m[][COLS], int r);
+
+int main(){
+    int a[4][3]={10,20,30,
+                 40,50,60,
+                 70,80,90,
+                 100,110,120};
+    double avg;
+    avg = get_row_avg(a,2);
+    printf("Row Average = %5.2f\n",avg);
+    return 0;
+}
